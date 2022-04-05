d96e034b12032c44a50b607bf745b475527bd0f0
unknown <you@example.com>
2022-04-05 15:54:35 +0600

    added reload page

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashBoard = () => {
     return (
         <div>
-            <h4>kdjfrugh</h4>
+           
         </div>
     );
 };
---
+++ src/components/Reload/Reload.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reload = () => {
+    return (
+        <div>
+            <h2>This page not found</h2>
+        </div>
+    );
+};
+
+export default Reload;
